cc90613e686e265961a00a33a741ecc986b536ac
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-03-06 18:07:36 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytaskbucketdana2"
+    bucket = "myawsbucketdana"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
